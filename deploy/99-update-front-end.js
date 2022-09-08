253712614fe81c5d74fd6cb905c3c040980b1cc7
Aishah Sofea <aishah.s.hanifa@gmail.com>
2022-09-09 00:31:07 +0800

    modify deploy script to update ABI on the FE

--- deploy/99-update-front-end.js
+++ deploy/99-update-front-end.js
@@ -5,13 +5,29 @@ require("dotenv").config();
 
 const frontEndContractsFile =
   "../nextjs-nft-marketplace-moralis-fcc/constants/networkMapping.json";
+const frontEndAbiLocation = "../nextjs-nft-marketplace-moralis-fcc/constants/";
 
 module.exports = async () => {
   if (process.env.UPDATE_FRONT_END) {
     console.log("Writing to front-end...");
     await updateContractAddresses();
+    await updateAbi();
   }
 };
+
+async function updateAbi() {
+  const nftMarketplace = await ethers.getContract("NftMarketplace");
+  fs.writeFileSync(
+    `${frontEndAbiLocation}NftMarketplace.json`,
+    nftMarketplace.interface.format(ethers.utils.FormatTypes.json)
+  );
+
+  const basicNft = await ethers.getContract("BasicNft");
+  fs.writeFileSync(
+    `${frontEndAbiLocation}BasicNft.json`,
+    basicNft.interface.format(ethers.utils.FormatTypes.json)
+  );
+}
 
 async function updateContractAddresses() {
   const chainId = network.config.chainId.toString();
